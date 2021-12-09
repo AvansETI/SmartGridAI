40d2d6f7919c2d3e8d9403d89ffaa6afc44240ab
Leander Oudakker <lfj.oudakker@student.avans.nl>
2021-12-09 12:08:50 +0100

    B4BUX-46 Add support for multiple input files for MLOps pipeline

--- B4B Human Wellbeing Prediction/mlops/pipeline.py
+++ B4B Human Wellbeing Prediction/mlops/pipeline.py
@@ -1,3 +1,4 @@
+import glob
 import shutil
 import time
 from datetime import datetime
@@ -14,50 +15,60 @@ def default_file_name():
     return datetime.today().strftime('%d-%m-%Y_%H:%M:%S')
 
 
-class ProcessData(luigi.Task):
-    file_path = luigi.Parameter(default='../dataset/dataset.csv')
-    output_path = luigi.Parameter(default=f'exported_datasets/{default_file_name()}.csv')
-    target_name = luigi.Parameter(default='acceptability_90')
+class CombineData(luigi.Task):
+    folder_path = luigi.Parameter(default='../dataset/datase*.csv')
+    output_path = luigi.Parameter(default=f'combined_datasets/{default_file_name()}.csv')
     time = luigi.Parameter(default=time.time())
 
     def output(self):
         return luigi.LocalTarget(self.output_path)
 
     def run(self):
-        df = pd.read_csv(self.file_path)
+        files = glob.glob(self.folder_path)
+
+        csv = pd.concat([pd.read_csv(file, index_col=[0]) for file in files])
+
+        csv.to_csv(self.output_path, index=False)
+
+
+class ProcessData(luigi.Task):
+    output_path = luigi.Parameter(default=f'processed_datasets/{default_file_name()}.csv')
+    target_name = luigi.Parameter(default='acceptability_90')
+    time = luigi.Parameter(default=time.time())
+
+    def requires(self):
+        return [CombineData()]
+
+    def output(self):
+        return luigi.LocalTarget(self.output_path)
+
+    def run(self):
+        file_path = self.input()[0].path
+
+        df = pd.read_csv(file_path)
 
         df = shuffle(df)
 
         df.rename(columns={self.target_name: 'target'}, inplace=True)
 
-        df['datetime'] = pd.to_datetime(df['datetime'])
+        df = pd.get_dummies(df, columns=['room'])
 
-        df['hour'] = df['datetime'].dt.hour
-        df['minute'] = df['datetime'].dt.minute
-        df['second'] = df['datetime'].dt.second
-
-        columns = [
-            'temperature',
-            'mean_temp_day',
-            'heatindex',
-            'room',
-            'relative_humidity',
-            'light_sensor_one_wavelength',
-            'light_sensor_two_wavelength',
-            'number_occupants',
-            'activity_occupants',
-            'door_state',
-            'hour',
-            'minute',
-            'second',
-            'target',
+        drop = [
+            'node_id',
+            'datetime',
+            'acceptability_80',
+            'tmp_cmf',
+            'tmp_cmf_80_low',
+            'tmp_cmf_80_up',
+            'tmp_cmf_90_low',
+            'tmp_cmf_90_up',
         ]
 
-        df[columns].to_csv(self.output_path)
+        df = df.drop(drop, axis=1)
+        df.to_csv(self.output_path)
 
 
 class OptimizeModel(luigi.Task):
-    file_path = luigi.Parameter(default=f'exported_datasets/{default_file_name()}.csv')
     output_path_pipeline = luigi.Parameter(default=f'exported_pipelines/{default_file_name()}.py')
     output_path_model = luigi.Parameter(default=f'exported_models/{default_file_name()}.pkl')
     scoring_function = luigi.Parameter(default=None)
@@ -68,27 +79,12 @@ class OptimizeModel(luigi.Task):
         return [ProcessData()]
 
     def run(self):
-        df = pd.read_csv(self.file_path)
+        file_path = self.input()[0].path
 
-        features = [
-            'temperature',
-            'mean_temp_day',
-            'heatindex',
-            'room',
-            'relative_humidity',
-            'light_sensor_one_wavelength',
-            'light_sensor_two_wavelength',
-            'number_occupants',
-            'activity_occupants',
-            'door_state',
-            'hour',
-            'minute',
-            'second',
-            'target',
-        ]
+        df = pd.read_csv(file_path)
 
         y = df['target']
-        X = df[features]
+        X = df.drop('target', axis=1)
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y.values, test_size=0.33)
 
@@ -110,7 +106,7 @@ class OptimizeModel(luigi.Task):
         self.set_status_message(f'Model score: {score}')
 
         if self.score_threshold < (score * 100):
-            pipeline_optimizer.export(self.output_path_pipeline, data_file_path=self.file_path)
+            pipeline_optimizer.export(self.output_path_pipeline, data_file_path=file_path)
 
             with open(self.output_path_model, 'wb') as file:
                 cloudpickle.dump(self, file)
