0e3e16223f61e806eb1bd3b0e85b9918605036be
maartendonkersloot <maartendonkersloot@gmail.com>
2022-01-14 12:40:53 +0100

    Fixed prediction data order

--- B4B Human Wellbeing Prediction/api/queries.py
+++ B4B Human Wellbeing Prediction/api/queries.py
@@ -10,17 +10,32 @@ def register(query):
     @query.field("predict")
     def predict_resolver(obj, info, input):
         prediction_data = {
-            "temperature": (input["temperature"] if "temperature" in input else sensors.get_temperature()),
-            "mean_temp_day": (input["mean_temp_day"] if "mean_temp_day" in input else sensors.get_mean_temp_day()),
+            "hour": (input["hour"] if "hour" in input else sensors.get_hour()),
+            "minute": (input["minute"] if "minute" in input else sensors.get_minute()),
+            "second": (input["second"] if "second" in input else sensors.get_second()),
+            "relative_time": (
+                input["relative_time"]
+                if "relative_time" in input
+                else sensors.get_relative_time()
+            ),
+            "temperature": (
+                input["temperature"]
+                if "temperature" in input
+                else sensors.get_temperature()
+            ),
+            "mean_temp_day": (
+                input["mean_temp_day"]
+                if "mean_temp_day" in input
+                else sensors.get_mean_temp_day()
+            ),
             "heat_index": (
                 input["heat_index"]
                 if "heat_index" in input
                 else (
-                    calculate_heat_index(input["temperature"], input["relative_humidity"])
-                    if (
-                        "temperature" in input
-                        and "relative_humidity" in input
+                    calculate_heat_index(
+                        input["temperature"], input["relative_humidity"]
                     )
+                    if ("temperature" in input and "relative_humidity" in input)
                     else sensors.get_heat_index()
                 )
             ),
@@ -51,19 +66,17 @@ def register(query):
             ),
             "state_of_door": (
                 (1 if input["state_of_door"] is True else 0)
-                if "state_of_door" in input else sensors.get_state_of_door()
+                if "state_of_door" in input
+                else sensors.get_state_of_door()
             ),
-            "hour": (input["hour"] if "hour" in input else sensors.get_hour()),
-            "minute": (input["minute"] if "minute" in input else sensors.get_minute()),
-            "second": (input["second"] if "second" in input else sensors.get_second()),
             "window_state": (
                 (1 if input["window_state"] is True else 0)
-                if "window_state" in input else sensors.get_window_state()
+                if "window_state" in input
+                else sensors.get_window_state()
             ),
             "room_A": ((1 if input["room"] == 0 else 0) if "room" in input else None),
             "room_B": ((1 if input["room"] == 1 else 0) if "room" in input else None),
             "room_C": ((1 if input["room"] == 2 else 0) if "room" in input else None),
-            "relative_time": (input["relative_time"] if "relative_time" in input else sensors.get_relative_time()),
         }
 
         if (
@@ -75,10 +88,9 @@ def register(query):
             prediction_data[f"room_{choice(['A', 'B', 'C'])}"] = 1
 
         predictor = Predictor()
+
         prediction_df = pd.DataFrame([list(prediction_data.values())])
-
         return {
-
             **(predictor.predict_model_proba(prediction_df)),
-            "shapOptions": json.dumps(predictor.plot(prediction_df).data)
+            "shapOptions": json.dumps(predictor.plot(prediction_df).data),
         }
